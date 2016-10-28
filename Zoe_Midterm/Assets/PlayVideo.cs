c73aa129e992a2f3ae8016deb1f2e5bfa173b728
Zoe Bachman <zoe.bachman@gmail.com>
2016-10-28 01:08:03 -0400

    got video files to play

---
+++ Zoe_Midterm/Assets/PlayVideo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+// this line of code will make the Movie Texture begin playing
+//(MovieTexture)GetComponent<Renderer>().material.mainTexture.Play();
+//RequireComponent(typeof(AudioSource))
+
+public class PlayVideo : MonoBehaviour {
+	public MovieTexture movie;
+	//private AudioSource audio;
+
+	void Start () {
+		GetComponent<RawImage> ().texture = movie as MovieTexture;
+//		audio = GetComponent<AudioSource> ();
+//		audio.clip = movie.audioClip;
+		movie.Play();
+//		audio.Play();
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space) && movie.isPlaying) 
+		{
+			movie.Pause ();
+		} else if(Input.GetKeyDown (KeyCode.Space) && !movie.isPlaying)
+		{
+			movie.Play();
+		}
+	}
+}
---
+++ Zoe_Midterm/Assets/movie_texture.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// this line of code will make the Movie Texture begin playing
+//(MovieTexture)GetComponent<Renderer>().material.mainTexture.Play();
+
+public class PlayMovieOnSpace : MonoBehaviour {
+	void Update () {
+		if (Input.GetButtonDown ("Jump")) {
+
+			Renderer r = GetComponent<Renderer>();
+			MovieTexture movie = (MovieTexture)r.material.mainTexture;
+
+			if (movie.isPlaying) {
+				movie.Pause();
+			}
+			else {
+				movie.Play();
+			}
+		}
+	}
+}
