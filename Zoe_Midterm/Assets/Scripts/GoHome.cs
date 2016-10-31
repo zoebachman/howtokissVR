4dacdf3cb474a3129c1141b511ac8918f73b9d87
Zoe Bachman <zoe.bachman@gmail.com>
2016-10-31 11:37:31 -0400

    re-uploaded scenes and scripts

---
+++ Zoe_Midterm/Assets/Scripts/GoHome.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoHome : MonoBehaviour {
+
+	public GameObject[] enviros;
+	//var home : GameObject;
+	int currentEnviroNum; //how can it know what enviro it's on?
+
+	//to use this script with GVR pugin, attach it to a game object in the scene
+	//add an event trigger to the game object
+	//add a pointerclick (or pointenter, if you prefer) to the event trigger
+	//drag the game object you attached this script into the game object field
+	//choose this script and whichever function you'd ike to use 
+
+	//function below sets the first item in the array to active, and the others to inactive
+	//can be used to switch environments or simple switch in and out objects
+	//to use, attach to gameobject and drag the items you would like to switch onto the array
+
+	public void ChangeEnviro(){
+		Debug.Log ("switched to" + currentEnviroNum);
+		enviros [currentEnviroNum].SetActive (false);
+		currentEnviroNum = 0;
+//		if (currentEnviroNum == enviros.Length) {
+//			currentEnviroNum = 0;
+//		}
+		enviros [0].SetActive (true);
+	}
+}
---
+++ Zoe_Midterm/Assets/Scripts/goNewScene.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goNewScene : MonoBehaviour {
+
+
+
+void Start () {
+	DontDestroyOnLoad(transform.gameObject);
+}
+
+
+public void goToScene1(){
+	Application.LoadLevel("scene1");
+}
+
+
+public void goToScene2(){
+	Application.LoadLevel("scene2");
+}
+
+	public void goToScene3(){
+		Application.LoadLevel("scene3");
+	}
+
+	public void goToScene4(){
+		Application.LoadLevel("scene4");
+	}
+}
